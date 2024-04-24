ea0482b7afa23b09292db5477272bbe0f5bf83ba
h_kuramoto <h.kuramoto@bravetechnology.co.jp>
2024-04-24 20:30:31 +0900

    投稿画面をウインドウにした

--- src/pages/users/[id]/index.vue
+++ src/pages/users/[id]/index.vue
@@ -7,17 +7,47 @@ v-container
         color="green"
         rounded="pill"
         height="100px"
-        :to="`/users/${lineId}/form`"
+        @click="overlay.form = true"
       ) {{ $t('button.post') }}
+        v-overlay(
+          v-model="overlay.form"
+        )
+          window-form(
+            @back="overlay.form = false"
+            @send="dialog.sendForm = true"
+          )
   // 過去の投稿一覧
   v-row
     v-col
-      p.text-center {{ $t('pages.users.index.post_list') }}
+      p.text-center {{ $t('pages.users.index.theme_list') }}
+
+// 送信確認
+dialog-send-form(
+  v-model="dialog.sendForm"
+  @ok="submit"
+)
 </template>
 
 <script setup lang="ts">
 const lineId = useState('lineId', () => '')
 lineId.value = 1
+
+const form = ref({
+  theme: ''
+})
+const overlay = ref({
+  form: false
+})
+const dialog = ref({
+  sendForm: false
+})
+
+// methods
+const submit = () => {
+  console.log('test')
+  dialog.value.sendForm = false
+  overlay.value.form = false
+}
 </script>
 
 <style scoped lang="sass">
