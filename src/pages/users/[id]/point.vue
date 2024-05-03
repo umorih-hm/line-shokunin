ffc1396c14785e308e02ae1cd5d1611818097c50
umorih~ <umorih.net@gmail.com>
2024-05-03 16:35:51 +0900

    ポイント画面のレイアウトを調整

--- src/pages/users/[id]/point.vue
+++ src/pages/users/[id]/point.vue
@@ -3,28 +3,28 @@ v-container
   // 獲得ポイント
   v-row.d-flex.flex-row.justify-center
     v-col.d-flex.flex-column(cols="6")
-      h3.text-center.text-red {{ $t('pages.users.point.current_point') }}
+      h3.text-center.text-red.pb-2 {{ $t('pages.users.point.current_point') }}
       v-img(
-        height="80px"
+        height="60px"
         src="/point_p.png"
       )
     v-col(cols="6")
-      div.bg-red.rounded-lg.ma-auto(style="height: 100px")
+      div.bg-red.rounded-lg.ma-auto.point-card(style="height: 100px")
         h1.text-center.point-number__text {{ form.points }}
-        p.text-end.px-4.pt-0.point-number-unit__text {{ $t('pages.users.point.current_point_unit') }}
+        span.font-weight-bold.point-number-unit__text {{ $t('pages.users.point.current_point_unit') }}
 
   // 投稿採用率
   v-row.d-flex.flex-row.justify-center
     v-col(cols="6")
-      h3.text-center.text-green {{ $t('pages.users.point.pass_rate') }}
+      h3.text-center.text-green.pb-2 {{ $t('pages.users.point.pass_rate') }}
       v-img(
-        height="80px"
+        height="60px"
         src="/point_mail.png"
       )
     v-col(cols="6")
-      div.bg-green.rounded-lg.ma-auto(style="height: 100px")
+      div.bg-green.rounded-lg.ma-auto.point-card(style="height: 100px")
         h1.text-center.point-number__text {{ Math.round(form.passedRate * 100) }}
-        h1.text-end.px-4.point-number-unit__text {{ $t('pages.users.point.pass_rate_unit') }}
+        span.font-weight-bold.point-number-unit__text {{ $t('pages.users.point.pass_rate_unit') }}
 
   v-row.d-flex.justify-center
     v-img(
@@ -63,5 +63,11 @@ onMounted(async () => {
   font-size: 60px
 
 .point-number-unit__text
-  font-size: 24px
+  font-size: 20px
+  position: absolute
+  top: 67%
+  right: 8%
+
+.point-card
+  position: relative
 </style>
