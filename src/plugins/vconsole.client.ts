86565b64b886444f012cd24c4f4b6ec2dfe8a11a
umorih~ <umorih.net@gmail.com>
2024-05-06 16:28:31 +0900

    vconsole なくした

--- src/plugins/vconsole.client.ts
+++ src/plugins/vconsole.client.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
-  if (useAppConfig().environment === 'development' || useAppConfig().environment === 'local') {
-    import('vconsole').then(({ default: VConsole }) => {
-      new VConsole()
-    })
-  }
+  // if (useAppConfig().environment === 'development' || useAppConfig().environment === 'local') {
+  //   import('vconsole').then(({ default: VConsole }) => {
+  //     new VConsole()
+  //   })
+  // }
 })
